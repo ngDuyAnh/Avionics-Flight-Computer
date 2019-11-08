96fb5ef5728933637d542e12362f925909b78603
Yurii Vasylkiv <yurii.vasylkiv@gmail.com>
2019-11-08 17:39:42 -0600

    CLion Debugger

--- AvionicsSoftware-AtollicProject/Src/main.c
+++ AvionicsSoftware-AtollicProject/Src/main.c
@@ -223,7 +223,7 @@ int main(void)
   * @retval None
   */
 void assert_failed(uint8_t *file, uint32_t line)
-{ 
+{
 
   /* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
