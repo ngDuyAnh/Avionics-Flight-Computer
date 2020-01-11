5c41fa7119717a8ccf9fc8f72407dd6c8a6c30b6
yura0202 <vasilkivyra@mail.ru>
2020-01-10 19:36:11 -0500

    fixed BAT scripts

--- AvionicsSoftware-AtollicProject/ENVIRONMENT.bat
+++ AvionicsSoftware-AtollicProject/ENVIRONMENT.bat
@@ -4,21 +4,21 @@ SET /A ARGC=0
 FOR %%x IN (%*) DO SET /A ARGC+=1
 echo ARGUMENTS PROVIDED: %ARGC%
 
-IF "%~4"==""      GOTO    :usage
-IF NOT "%~5"==""  GOTO    :usage
+IF "%~4"=="" GOTO:usage
+IF NOT "%~5"=="" GOTO:usage
 
 SET MINGW_ENV_PATH=%1
 SET CLION_PATH=%2
 SET CLION_CMAKE_PATH=%3
 SET ARM_TOOLS_PATH=%4
 
-GOTO    :addToARMVariable
-goto    :EOF
+GOTO:addToARMVariable
+goto:EOF
 
 :addToARMVariable
     SETX ARM_DEV_ENV "%MINGW_ENV_PATH%;%CLION_PATH%;%CLION_CMAKE_PATH%;%ARM_TOOLS_PATH%;%ARM_DEV_ENV%"
     FOR /F "tokens=2* delims= " %%f IN ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path ^| findstr /i path') DO SET OLD_SYSTEM_PATH=%%g
-    SETX Path "%OLD_SYSTEM_PATH%;%ARM_DEV_ENV%%"
+    SETX Path "%OLD_SYSTEM_PATH%;%%ARM_DEV_ENV%%"
     exit /b
 
 :usage
