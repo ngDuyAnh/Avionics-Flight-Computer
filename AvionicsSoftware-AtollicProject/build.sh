a65fd7fdb12ee79fbd27a25f45a566ae15f547be
Yurii Vasylkiv <yurii.vasylkiv@gmail.com>
2020-01-18 01:17:22 -0800

    git structure change, fixed the scripts, created extensive README, removed CONFIGURATION scripts

--- AvionicsSoftware-AtollicProject/build.sh
+++
@@ -1,8 +0,0 @@
-rm -rf cmake-build-build
-rm -rf bin
-mkdir build
-cd build || return
-cmake .. -G "CodeBlocks - Unix Makefiles"
-make -j8
-cd ..
-
--- AvionicsSoftware-AtollicProject/clean.sh
+++
@@ -1,5 +0,0 @@
-rm -rf ./build
-rm -rf ./bin
-rm -rf ./generated
-rm REBUILD.bat
-rm REBUILD.ps1
