647b14c7926cbf58b388b792d24dcc35af27f290
Yurii Vasylkiv <yurii.vasylkiv@gmail.com>
2020-01-29 17:13:03 -0800

    added serialization functions

--- Src/utilities/common.h
+++ Src/utilities/common.h
@@ -6,6 +6,38 @@
 #include <stdbool.h>
 #include <string.h>
 
+#define DESERIALIZE(type, size)                             \
+    static inline type to_##type(uint8_t* src){             \
+        union {                                             \
+            type value;                                     \
+            char bytes[size];                               \
+        } u;                                                \
+        memcpy(u.bytes, src, 4);                            \
+        return u.value;                                     \
+    }                                                       \
+
+#define SERIALIZE(type, size)                                   \
+    static inline void from_##type(type value, uint8_t* buf){   \
+        union {                                                 \
+            type value;                                         \
+            char bytes[size];                                   \
+        } u;                                                    \
+        u.value = value;                                        \
+        memcpy(buf, u.bytes, 4);                                \
+    }                                                           \
+
+
+SERIALIZE(double, 4)
+SERIALIZE(float, 4)
+SERIALIZE(uint32_t, 4)
+SERIALIZE(int32_t, 4)
+
+DESERIALIZE(double, 4)
+DESERIALIZE(float,4)
+DESERIALIZE(uint32_t,4)
+DESERIALIZE(int32_t,4)
+
+
 static inline void write_32(uint32_t src, uint8_t * dest)
 {
     dest[0] = (uint8_t) ((src >> 24) & 0xFF);
